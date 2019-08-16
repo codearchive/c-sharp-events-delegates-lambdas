0271360f5858b9656484f1c4f9ea49214fce7119
Pavel Patrusov <pavelp9415@gmail.com>
2019-08-16 20:42:00 +0300

    Completed unit 2.9

--- module_02/module_02/Worker.cs
+++ module_02/module_02/Worker.cs
@@ -11,6 +11,41 @@ namespace module_02
 
         public void DoWork(int hours, WorkType workType)
         {
+            for (int i = 0; i < hours; i++)
+            {
+                OnWorkPerformed(i + 1, workType);
+            }
+
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //if (WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours, workType)
+            //}
+
+            var del = WorkPerformed as WorkPerfomedHandler;
+            if (del != null)
+            {
+                del(hours, workType);
+            }
+
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            //if (WorkCompleted != null)
+            //{
+            //    WorkCompleted(hours, workType)
+            //}
+
+            var del = WorkCompleted as EventHandler;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
 
         }
     }
