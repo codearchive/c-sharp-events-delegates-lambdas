d86791a7fdaff52cf3de6bfd7d55db4f95d8ebfc
Pavel Patrusov <pavelp9415@gmail.com>
2019-08-17 12:07:47 +0300

    Completed unit 3.3

---
+++ module_03/module_03/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace module_03
+{
+    class Program
+    {
+        static void Main()
+        {
+            //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
+
+            //del1(5, WorkType.Golf);
+            //del2(10, WorkType.GenerateReports);
+
+            //DoWork(del1);
+
+            //del1 += del2 + del3; // invocation list -> del1, del2, del3 will be printed in that order
+
+            //int finalHours = del1(10, WorkType.GenerateReports);
+
+            var worker = new Worker();
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(worker_WorkPerformed);
+            worker.WorkCompleted += new EventHandler(worker_WorkCompleted);
+            worker.DoWork(8, WorkType.GenerateReports);
+            //Console.WriteLine(finalHours);
+        }
+
+        static void worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine($"Hours worked: {e.Hours} {e.WorkType}");
+        }
+
+        static void worker_WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Worker is done");
+        }
+
+        //static void DoWork(WorkPerformedHandler del)
+        //{
+        //    del(5, WorkType.GoToMeetings);
+        //}
+
+        //static int WorkPerformed1(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed1 called " + hours.ToString());
+        //    return hours + 1;
+        //}
+
+        //static int WorkPerformed2(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed2 called " + hours.ToString());
+        //    return hours + 2;
+        //}
+
+        //static int WorkPerformed3(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed3 called " + hours.ToString());
+        //    return hours + 3;
+        //}
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
---
+++ module_03/module_03/Worker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace module_03
+{
+    //public delegate int WorkPerformedHandler(object sender, WorkPerformedEventArgs e); // Comment #001 - First way to create custom EventArgs.
+
+    public class Worker
+    {
+        //public event WorkPerformedHandler WorkPerformed; // Comment #001 - First way to create custom EventArgs.
+        public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
+        public event EventHandler WorkCompleted;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                OnWorkPerformed(i + 1, workType);
+            }
+
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //if (WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours, workType)
+            //}
+
+            // var del = WorkPerformed as WorkPerformedHandler; // Comment #001 - First way to create custom EventArgs.
+            var del = WorkPerformed as EventHandler<WorkPerformedEventArgs>;
+            if (del != null)
+            {
+                del(this, new WorkPerformedEventArgs(hours, workType));
+            }
+
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            //if (WorkCompleted != null)
+            //{
+            //    WorkCompleted(hours, workType)
+            //}
+
+            var del = WorkCompleted as EventHandler;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
+
+        }
+    }
+}
