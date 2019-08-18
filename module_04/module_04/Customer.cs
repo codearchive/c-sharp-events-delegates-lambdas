54b7993c03af7cbc4f77b7313c8be599fce9c23e
Pavel Patrusov <pavelp9415@gmail.com>
2019-08-18 21:23:21 +0300

    Completed unit 4.9

---
+++ module_04/module_04/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace module_04
+{
+    class Customer
+    {
+        public int  ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
--- module_04/module_04/Program.cs
+++ module_04/module_04/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace module_04
 {
@@ -8,30 +10,49 @@ namespace module_04
     {
         static void Main()
         {
-            BizRulesDelegate addDel = (x, y) => x + y;
-            BizRulesDelegate multiplyDel = (x, y) => x * y;
+            // Unit [4.1, 4.8]
 
-            var data = new ProcessData();
-            data.Process(2, 3, multiplyDel);
+            //BizRulesDelegate addDel = (x, y) => x + y;
+            //BizRulesDelegate multiplyDel = (x, y) => x * y;
 
-            // Action<T>
+            //var data = new ProcessData();
+            //data.Process(2, 3, multiplyDel);
 
-            Action<int, int> myAction = (x, y) => Console.WriteLine(x + y);
-            Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine(x * y);
-            data.ProcessAction(5, 7, myMultiplyAction);
+            //// Action<T>
 
-            // Func<T, TRes>
+            //Action<int, int> myAction = (x, y) => Console.WriteLine(x + y);
+            //Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine(x * y);
+            //data.ProcessAction(5, 7, myMultiplyAction);
 
-            Func<int, int, int> funcAddDel = (x, y) => x + y;
-            Func<int, int, int> funcMultiplyDel = (x, y) => x * y;
-            data.ProcessFunc(2, 3, funcAddDel);
+            //// Func<T, TRes>
+
+            //Func<int, int, int> funcAddDel = (x, y) => x + y;
+            //Func<int, int, int> funcMultiplyDel = (x, y) => x * y;
+            //data.ProcessFunc(2, 3, funcAddDel);
+
+            var custs = new List<Customer>
+            {
+                new Customer { City = "Phoenix", FirstName = "John", LastName = "Doe", ID = 1},
+                new Customer { City = "Phoenix", FirstName = "Jane", LastName = "Doe", ID = 500},
+                new Customer { City = "Seattle", FirstName = "Suki", LastName = "Pizzoro", ID = 3},
+                new Customer { City = "New York City", FirstName = "Michelle", LastName = "Smith", ID = 4},
+            };
+
+            var phxCusts = custs
+                .Where(c => c.City == "Phoenix" && c.ID < 500)
+                .OrderBy(c => c.FirstName);
+
+            foreach (var cust in phxCusts)
+            {
+                Console.WriteLine($"{cust.FirstName}, {cust.LastName}");
+            }
 
             var worker = new Worker();
             worker.WorkPerformed += (s, e) =>
             {
                 Console.WriteLine($"Hours worked: {e.Hours} {e.WorkType}");
                 Console.WriteLine($"Test lambda {e.Hours}"); // Testing opportunities to add multiple lines of code.
-            }; 
+            };
             worker.WorkCompleted += (s, e) => Console.WriteLine("Worker is done");
 
             worker.DoWork(4, WorkType.GenerateReports);
